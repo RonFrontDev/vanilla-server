2ebcf970244b3d3b49b01b6771e0158df9b800e8
Ronny Christensen <ronny.christensen@bookingboard.io>
2024-09-06 14:16:33 +0200

    created Html

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 import http from "http";
+import fs from "fs";
 
 const port = 4000;
 const server = http.createServer(handleRequest);
 
-function handleRequest(req, res) {
-  console.log("en lille log");
-  res.writeHead(200, { "content-Type": "text/plain" });
-  res.write("hallo world");
-  res.end("ByeBye world");
+function handleRequest(error, data) {
+  fs.readFile("index.html", handleError);
+}
+
+function (error, data) {
+  if (error) {
+    res.writeHead(404,{"Content-Type": "text/plain"});
+    res.write("Error:file not found");
+  } else {
+    res.writeHead(200,{"Content-Type":"text/html"})
+    res.write(data);
+  }
+  res.end();
 }
 
 function onConnect(error) {
