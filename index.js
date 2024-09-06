e3b71da5b4cc3f9ea18c833a91d774fcdf2b723e
Ronny Christensen <ronny.christensen@bookingboard.io>
2024-09-06 13:57:13 +0200

    log

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const port = 4000;
 const server = http.createServer(handleRequest);
 
 function handleRequest(req, res) {
+  console.log("en lille log");
   res.writeHead(200, { "content-Type": "text/plain" });
   res.write("hallo world");
   res.end("ByeBye world");
