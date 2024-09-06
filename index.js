513081fb00d1b7a98b97aad4c01e0292fec9888e
Ronny Christensen <ronny.christensen@bookingboard.io>
2024-09-06 13:45:40 +0200

    server setup

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("First drive");
+import http from http
+
+const port = 4000
+const server = http.createServer(handleRequst)
+
+function handleRequst(req,res) {
+    res.writeHead(200, {"content-Type":  "text/plain"})
+    res.write("hallo world")
+    res.end("ByeBye world")
+}
+
+function onConnect(error) {
+    if (error) {
+        console.log("server bad")
+    }else {
+        console.log("server good "+ port )
+    }
+}
+
+server.listen(port, onConnect)
