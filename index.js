239046ad2c1e83194bb0a40d5146a6eb520ae6a4
Ronny Christensen <ronny.christensen@bookingboard.io>
2024-09-06 13:49:23 +0200

    fix

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-import http from http
+import http from "http";
 
-const port = 4000
-const server = http.createServer(handleRequst)
+const port = 4000;
+const server = http.createServer(handleRequest);
 
-function handleRequst(req,res) {
-    res.writeHead(200, {"content-Type":  "text/plain"})
-    res.write("hallo world")
-    res.end("ByeBye world")
+function handleRequest(req, res) {
+  res.writeHead(200, { "content-Type": "text/plain" });
+  res.write("hallo world");
+  res.end("ByeBye world");
 }
 
 function onConnect(error) {
-    if (error) {
-        console.log("server bad")
-    }else {
-        console.log("server good "+ port )
-    }
+  if (error) {
+    console.log("server bad");
+  } else {
+    console.log("server good " + port);
+  }
 }
 
-server.listen(port, onConnect)
+server.listen(port, onConnect);
