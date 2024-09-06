da5e5da6e13b0e4bd080d90eb9c76a84fcf6c1b6
Ronny Christensen <ronny.christensen@bookingboard.io>
2024-09-06 14:25:15 +0200

    functions rewrite

--- index.js
+++ index.js
@@ -2,29 +2,24 @@ import http from "http";
 import fs from "fs";
 
 const port = 4000;
-const server = http.createServer(handleRequest);
 
-function handleRequest(error, data) {
-  fs.readFile("index.html", handleError);
-}
+const server = http.createServer((req, res) => {
+  fs.readFile("index.html", (error, data) => {
+    if (error) {
+      res.writeHead(404, { "Content-Type": "text/plain" });
+      res.write("Error: file not found");
+    } else {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(data);
+    }
+    res.end();
+  });
+});
 
-function (error, data) {
-  if (error) {
-    res.writeHead(404,{"Content-Type": "text/plain"});
-    res.write("Error:file not found");
-  } else {
-    res.writeHead(200,{"Content-Type":"text/html"})
-    res.write(data);
-  }
-  res.end();
-}
-
-function onConnect(error) {
+server.listen(port, (error) => {
   if (error) {
     console.log("server bad");
   } else {
     console.log("server good " + port);
   }
-}
-
-server.listen(port, onConnect);
+});
